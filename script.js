5f73d2a45e1687e854d37ddde3de0044fd6956d5
Jack112-create <jackconroy777@gmail.com>
2021-04-10 10:48:15 +0100

    Added currency

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const container = document.querySelector("#container");
 container.addEventListener("input", update);
 
+function formatMoney(value) {
+  value = Math.ceil(value * 100) / 100;
+  value = value.toFixed(2);
+  return `$${value}`;
+}
+
+function formatSplit(value) {
+  if (value === "1") {
+    return value + " person";
+  } else {
+    return value + " people";
+  }
+}
 function update() {
   let bill = Number(document.querySelector("#yourBill").value);
   let tipPercent = document.querySelector("#tipInput").value;
@@ -9,12 +22,13 @@ function update() {
   let tipValue = bill * (tipPercent / 100);
   let tipEach = tipValue / split;
   let newBillEach = (bill + tipValue) / split;
-  console.log(tipValue);
 
   document.getElementById("tipPercent").innerHTML = `${tipPercent}%`;
-  document.getElementById("tipValue").innerHTML = tipValue;
-  document.getElementById("totalWithTip").innerHTML = bill + tipValue;
-  document.getElementById("splitValue").innerHTML = split;
-  document.getElementById("billEach").innerHTML = newBillEach;
-  document.getElementById("tipEach").innerHTML = tipEach;
+  document.getElementById("tipValue").innerHTML = formatMoney(tipValue);
+  document.getElementById("totalWithTip").innerHTML = formatMoney(
+    bill + tipValue
+  );
+  document.getElementById("splitValue").innerHTML = formatSplit(split);
+  document.getElementById("billEach").innerHTML = formatMoney(newBillEach);
+  document.getElementById("tipEach").innerHTML = formatMoney(tipEach);
 }
