d7be8ec6e07307566420684db9d8cdc12a4304a3
Jack112-create <jackconroy777@gmail.com>
2021-04-10 10:13:16 +0100

    Added calculation

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const container = document.querySelector("#container");
 container.addEventListener("input", update);
 
 function update() {
-  let bill = document.querySelector("#yourBill").value;
+  let bill = Number(document.querySelector("#yourBill").value);
   let tipPercent = document.querySelector("#tipInput").value;
-  let splitInput = document.querySelector("#splitInput").value;
-  //let tipSpan = document.querySelector("#tipPercent");
+  let split = document.querySelector("#splitInput").value;
 
-  /*
-  if (tipPercent >= 0) {
-    tipSpan.textContent = `${tipPercent++}%`;
-  }
-   */
-  console.log({ bill, tipPercent, splitInput });
+  let tipValue = bill * (tipPercent / 100);
+  let tipEach = tipValue / split;
+  let newBillEach = (bill + tipValue) / split;
+  console.log(tipValue);
+
+  document.getElementById("tipPercent").innerHTML = `${tipPercent}%`;
+  document.getElementById("tipValue").innerHTML = tipValue;
+  document.getElementById("totalWithTip").innerHTML = bill + tipValue;
+  document.getElementById("splitValue").innerHTML = split;
+  document.getElementById("billEach").innerHTML = newBillEach;
+  document.getElementById("tipEach").innerHTML = tipEach;
 }
